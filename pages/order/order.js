46737bccd20029096a2cb35b6f0255a5c7313de0
2359634711 <2359634711@qq.com>
2019-07-01 17:44:07 +0800

    7-1

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,6 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
+    currentShowPageId: 0,
+    navHeaderList: [
+      {
+        title: '全部',
+        selected: true,
+        id: 0
+      }, {
+        title: '待付款',
+        selected: true,
+        id: 1
+      }, {
+        title: '待发货',
+        selected: true,
+        id: 2
+      }, {
+        title: '待收货',
+        selected: true,
+        id: 3
+      }, {
+        title: '已完成',
+        selected: true,
+        id: 4
+      }, {
+        title: '已取消',
+        selected: true,
+        id: 5
+      }
+    ]
 
   },
 
@@ -12,7 +40,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '我的订单'
+    })
+  },
+  changeNav(item){
+    let id = item.currentTarget.dataset.id;
+    this.setData({
+      currentShowPageId: id
+    })
+    console.log(this.data.currentShowPageId)
   },
 
   /**
